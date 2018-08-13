415f6095c6db7c978835e0fa85c6966425dd792e
Benson Giang <bensongiang@gmail.com>
2018-08-13 15:49:39 -0700

    added sourcemap for webpack and setup production

--- src/app.js
+++ src/app.js
@@ -4,15 +4,12 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import getVisibleExpenses from './selectors/expenses';
-import { addExpense, removeExpense, editExpense } from './actions/expenses';
-import { setTextFilter, setStartDate, setEndDate, sortByAmount, sortByDate } from './actions/filters';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 
 
